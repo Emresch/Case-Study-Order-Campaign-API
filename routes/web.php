683d04daad6f2e7f2e87fe9d382fc7b73c9e3733
Emresch <emresenturk4@outlook.com>
2026-07-28 15:08:02 +0300

    Auth ve Api-key ile ilgili düzenlemeler yapıldı.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('api-key')->group(function () {
+    Route::post('/orders', [OrderController::class, 'store']);
+    Route::get('/orders/{id}', [OrderController::class, 'index']);
+
+    
 });
